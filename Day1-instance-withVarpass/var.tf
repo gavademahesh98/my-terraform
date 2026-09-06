ff553968afa7180f261b72543b29b843f5be9fbe
Mahesh Gavade <gavademahesh98@gmail.com>
2026-09-06 20:19:15 +0530

    added varwith instance

---
+++ Day1-instance-withVarpass/var.tf
@@ -0,0 +1,16 @@
+variable "ami"{
+    default= "ami-0532913178263be11"
+}
+
+variable "instance_type" {
+    default = "c7i-flex.large"
+}
+
+variable "key_name"{
+    default = "key-singapore"
+
+}
+
+variable "instance_name"{
+    default = "varby-instance"
+}
