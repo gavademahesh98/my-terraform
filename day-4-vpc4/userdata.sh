27d6b984f3c5cb828a48a89a311b4b477940d6e4
Mahesh Gavade <gavademahesh98@gmail.com>
2026-09-09 00:06:44 +0530

    Refactor userdata script to improve Nginx installation and configuration

--- day-4-vpc4/userdata.sh
+++ day-4-vpc4/userdata.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-apt update -y
-apt install nginx -y
-echo "<h1> Hello From $HOSTNAME </h1>" > /var/www/html/index.html
+set -e
+
+apt-get update -y
+apt-get install -y nginx
+
+systemctl enable nginx
+systemctl start nginx
+
+echo "<h1>Hello From $HOSTNAME</h1>" > /var/www/html/index.html
